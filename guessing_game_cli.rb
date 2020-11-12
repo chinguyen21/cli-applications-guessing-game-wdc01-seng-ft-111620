6d454df92d996dfb032fabe2f10d6a210a7f078c
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-11-12 18:49:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,13 +2,16 @@
 
 def run_guessing_game
   random_number = rand(1..6)
-  user_input = gets.chomp 
-  if user_input == "exit"
+  user_input = gets.chomp
+  if user_input == random_number.to_s
+    puts "You guessed the correct number!" 
+  elsif user_input == "exit"
     puts "Goodbye!"
-  elsif user_input == random_number.to_s
-    puts "You guessed the correct number!"
+  
   else
     puts "Sorry! The computer guessed #{random_number}."
   end 
   
 end
+
+  
