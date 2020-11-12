7328b41635410699c504c3d5a3d0869d11ed3a5b
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-11-12 18:53:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     puts "You guessed the correct number!" 
   elsif user_input == "exit"
     puts "Goodbye!"
-  
+
   else
     puts "Sorry! The computer guessed #{random_number}."
   end 
